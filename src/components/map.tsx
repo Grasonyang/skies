a47037fcdea26997ec89b12fc9541f45408a051a
grasonjas <grasonjas@gmail.com>
2025-10-04 07:40:12 +0000

    Changes before Firebase Studio auto-run

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, MapCameraChangedEvent } from "@vis.gl/react-google-maps";
 
 const MapComponent = () => {
   const position = { lat: 38.7946, lng: -106.5348 };
 
   return (
-    <APIProvider apiKey="AIzaSyAP55TtaAsZcw-v5MEdn_4lcBUQ9F3vlR0">
-      <div style={{ height: "400px" }}>
-        <Map zoom={4} center={position} />
-      </div>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+       <Map
+          defaultZoom={13}
+          defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+          onCameraChanged={ (ev: MapCameraChangedEvent) =>
+            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+          }>
+      </Map>
     </APIProvider>
   );
 };
